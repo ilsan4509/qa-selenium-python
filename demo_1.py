df1c29fcad2086c98fcc9592dfd9e93140ff3607
Seonggon <ilsan4509@naver.com>
2025-03-10 22:47:16 +0000

    Fix typos in basic concepts

---
+++ demo_1.py
@@ -0,0 +1,19 @@
+print("hello")
+
+# This is a comment
+
+a = 3
+print (a)
+
+Str = "Hello world"
+print(Str)
+
+b, c, d = 5, 6.4, "Good"
+print(a, c, d)
+
+#print("Value is"+b) can only concatenate str (not "int") to str
+
+print("{} {}".format("Value is", b))
+print("b type is", type(b))
+print("{} {}".format("c type is", type(c)))
+print("{} {}".format("d type is", type(d)))
