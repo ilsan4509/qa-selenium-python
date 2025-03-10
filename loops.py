7e9fb436d498c07d5d2f12511e47ae467ac1d603
Seonggon <ilsan4509@naver.com>
2025-03-10 23:56:54 +0000

    Add loops

---
+++ loops.py
@@ -0,0 +1,50 @@
+# Loops #
+
+# if else #
+greeting = "Good Morning"
+#Indentation is important in Python (들여쓰기 중요)
+print("1. if else - greeting")
+if greeting == "Morning":
+    print("Condition Matches")
+    print("second line")
+else:
+    print("condition do not match")
+
+print("if else condition code is completed")
+
+print("2. if else - start")
+a = 3
+if a > 2:
+    print("Condition Matches")
+    print("second line")
+else:
+    print("condition do not match")
+
+
+print("if else condition code is completed")
+
+# for loop #
+print("3. for - obj")
+obj = [2, 3, 5, 7, 9]
+for i in obj:
+    print(i*2)
+
+#sum of First Natural numbers 1+2+3+4+5 = 15
+#range(i,j) -> i to j-1
+print("4. for - summation")
+summation = 0
+#for(i=0; i<5; i+1) - Java
+for j in range(1, 6):
+    summation = summation + j
+    print(summation)
+print("Final summation =", summation)
+
+print("5. for - k")
+for k in range(1, 10, 2):
+    print(k)
+print("Final k =", k)
+
+print("5. for - m")
+for m in range(10):
+    print(m)
+print("Final m =", m)
