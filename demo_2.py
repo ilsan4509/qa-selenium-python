528a1d4c2ff30cb193e0b6ab87e01aabc1a90bf5
Seonggon <ilsan4509@naver.com>
2025-03-10 23:09:45 +0000

    Update data types

--- demo_2.py
+++ demo_2.py
@@ -1,4 +1,4 @@
-# Python String Data Type #
+# Data Type - String #
 e = "string in a double quote"
 f = 'string in a single quote'
 print(e)
@@ -10,10 +10,9 @@ print(e, "concatenated with", f)
 print(e + " concated with " + f)
 
 
-# Python List Data Type #
-values = [1,2, "paper", 4, 5]
-
+# Data Type - List #
 #List is data type that allows multiple values and can be different data types
+values = [1,2, "paper", 4, 5]
 
 print(values[0]) #1
 print(values[3]) #4
@@ -25,3 +24,30 @@ print(values)
 
 values.append("end") #[1, 2, 'paper', 'stone', 4, 5, 'end']
 print(values)
+
+values[2] = "PAPER" #[1, 2, 'PAPER', 'stone', 4, 5, 'end']
+print(values)
+
+del values[0] #[2, 'PAPER', 'stone', 4, 5, 'end']
+print(values)
+
+
+# Data Type - Tuple #
+#Same as List Data type but immutable
+val = (1, 2, "paper", 4.5)
+print(val) #(1, 2, 'paper', 4.5)
+print(val[1]) #2
+# val[1] = "PAPER"  #Tuples don't support item assignment
+
+# Data Type - Dictionary #
+dic = {"a":2, 4:"bcd", "e":"Hello"}
+
+print(dic[4]) #bcd
+print(dic["e"]) #Hello
+
+dict = {} #empty dictionary
+dict["FistName"] = "Seonggon"
+dict["LastName"] = "Kim"
+
+print(dict)#{'FistName': 'Seonggon', 'LastName': 'Kim'}
+print(dict["LastName"])#Kim
