094d1312fbe14cb2ee040f26da09d423e5f125ad
Seonggon <ilsan4509@naver.com>
2025-03-11 04:43:40 +0000

    Add functions

---
+++ functions.py
@@ -0,0 +1,24 @@
+#In Python, function is a group of related statements that perform a specific task.
+#Function Declaration
+def greet_me():
+    print("Good Morning")
+    #Function Call
+greet_me() #Good Morning
+
+def greet_you(name):
+    print("Good Morning, "+name)
+    #Function Call
+greet_you("Seonggon") #Good Morning
+
+def add_integers(a, b):
+    print(a+b)
+add_integers(2, 3)
+
+def add_integers_return(a, b):
+    return a+b
+print(add_integers_return(2, 3))
+
+
+
+
+
