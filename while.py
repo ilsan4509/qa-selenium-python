eccea31b231af6b4defed84b4da2882b6a831e6d
Seonggon <ilsan4509@naver.com>
2025-03-11 00:09:40 +0000

    Add while

---
+++ while.py
@@ -0,0 +1,21 @@
+# while #
+it = 10
+print("1. while - it")
+while it > 1:
+    if it == 9:
+        it = it -1
+        continue
+
+    if it != 7:
+        print(it)
+
+    if it == 3:
+        break
+
+    it = it - 1
+
+print("while loop execution is done")
+
+
+
+
