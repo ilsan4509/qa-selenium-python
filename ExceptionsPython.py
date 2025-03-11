c13aa1cc4bbd3a9e55755b3eabf1518fd40a154d
Seonggon <ilsan4509@naver.com>
2025-03-11 19:51:43 +0000

    Add exception - try, catch

--- ExceptionsPython.py
+++ ExceptionsPython.py
@@ -1,4 +1,6 @@
-#조건이 충족되지 않을 경우, 명시적으로 오류를 만들 수 있는 두가지 방법이 있다.
+#(조건이 충족되지 않을 경우, 명시적으로 오류를 만들 수 있는 두가지 방법)
+#Two ways to explicitly raise an error when a condition is not met
+
 #raise Exception, assert
 
 ItemsInCart = 0
@@ -9,5 +11,21 @@ if ItemsInCart != 2:
     #(assert)
     pass
 
-assert(ItemsInCart == 2)
+assert(ItemsInCart == 0)
 
+
+#try, catch
+try:
+    with open('wrong_name.txt', 'r') as reader:
+        reader.read()
+
+except:
+    print("Some how i reached this block because there is failure in try block")
+
+
+try:
+    with open('wrong_name.txt', 'r') as reader:
+        reader.read()
+
+except Exception as e: #Python error message
+    print(e)
