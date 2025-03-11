b3de5e95af97b4d2cdee9c263eafdb63fae33fae
Seonggon <ilsan4509@naver.com>
2025-03-11 22:09:52 +0000

    Add website loading automation - Firefox, Edge

--- demoBrowser.py
+++ demoBrowser.py
@@ -3,8 +3,18 @@ from selenium import webdriver
 #Chrome driver
 from selenium.webdriver.chrome.service import Service
 
+# -- Chrome
 service_obj = Service(r"C:\Users\ilsan\chromedriver-win64\chromedriver.exe")
 driver = webdriver.Chrome(service = service_obj)
+
+# -- Firefox
+# service_obj = Service(r"C:\Users\ilsan\geckodriver-v0.35.0-win64\geckodriver.exe")
+# driver = webdriver.Firefox(service = service_obj)
+
+# -- MicroEdge
+# service_obj = Service(r"C:\Users\ilsan\edgedriver_win64\msedgedriver.exe")
+# driver = webdriver.Edge(service = service_obj)
+
 driver.maximize_window()
 driver.get("https://rahulshettyacademy.com/")
 print(driver.title)
