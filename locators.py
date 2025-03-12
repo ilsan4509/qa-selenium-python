a96ca2bc523cd2135db6daa656dfe226a8c269dc
Seonggon <ilsan4509@naver.com>
2025-03-12 22:56:23 +0000

    Add CSS selectors and name locators

--- locators.py
+++ locators.py
@@ -1,3 +1,4 @@
+from pyexpat.errors import messages
 from selenium import webdriver
 
 #Chrome driver
@@ -14,3 +15,12 @@ driver.get("https://rahulshettyacademy.com/angularpractice/")
 driver.find_element(By.NAME, "email").send_keys("hello@gmail.com")
 driver.find_element(By.ID, "exampleInputPassword1").send_keys("123456")
 driver.find_element(By.ID, "exampleCheck1").click()
+
+# Xpath - //tagname[@attribute='value'] -> //input[@type='submit']
+# CSS - //tagname[@attribute='value'] -> //input[@type='submit']
+driver.find_element(By.CSS_SELECTOR, "input=[name='name']").send_keys("Rahul")
+driver.find_element(By.XPATH, "//input[@type='submit']").click()
+message = driver.find_element(By.CLASS_NAME, "alert-success").text
+
+print(message)
+assert "Success" in message
