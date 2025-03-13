e48916cb8f691c852b7cd0aa4881a2ddb010e85f
Seonggon <ilsan4509@naver.com>
2025-03-13 23:04:03 +0000

    Add Finding elements using XPath and CSS with the ChroPath plugin

--- locators.py
+++ locators.py
@@ -12,15 +12,19 @@ driver = webdriver.Chrome(service = service_obj)
 driver.get("https://rahulshettyacademy.com/angularpractice/")
 
 # ID, Xpath, CSSSelector, Classname, name, linkText
-driver.find_element(By.NAME, "email").send_keys("hello@gmail.com")
+driver.find_element(By.NAME, "email").send_keys("jjjayden@gmail.com")
 driver.find_element(By.ID, "exampleInputPassword1").send_keys("123456")
 driver.find_element(By.ID, "exampleCheck1").click()
 
 # Xpath - //tagname[@attribute='value'] -> //input[@type='submit']
 # CSS - //tagname[@attribute='value'] -> //input[@type='submit']
-driver.find_element(By.CSS_SELECTOR, "input=[name='name']").send_keys("Rahul")
+driver.find_element(By.CSS_SELECTOR, "input[name='name']").send_keys("jjjayden")
+driver.find_element(By.CSS_SELECTOR, "#inlineRadio1").click()
 driver.find_element(By.XPATH, "//input[@type='submit']").click()
 message = driver.find_element(By.CLASS_NAME, "alert-success").text
 
 print(message)
 assert "Success" in message
+
+driver.find_element(By.XPATH,"(//input[@type='text'])[3]").send_keys("222")
+driver.find_element(By.XPATH,"(//input[@type='text'])[3]").clear()
