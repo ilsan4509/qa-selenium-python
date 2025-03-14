ae3fe7b34ef13a7d91fae260da917567ee1f5579
Seonggon <ilsan4509@naver.com>
2025-03-14 22:01:44 +0000

    Add comments about Chrom Plugin - ChroPath

--- locators.py
+++ locators.py
@@ -15,16 +15,16 @@ driver.get("https://rahulshettyacademy.com/angularpractice/")
 driver.find_element(By.NAME, "email").send_keys("jjjayden@gmail.com")
 driver.find_element(By.ID, "exampleInputPassword1").send_keys("123456")
 driver.find_element(By.ID, "exampleCheck1").click()
-
+# Chrome Plugin - ChroPath
 # Xpath - //tagname[@attribute='value'] -> //input[@type='submit']
-# CSS - //tagname[@attribute='value'] -> //input[@type='submit']
-driver.find_element(By.CSS_SELECTOR, "input[name='name']").send_keys("jjjayden")
+# CSS - //tagname[@attribute='value'] -> //input[@type='submit'], #id,  .classname
+driver.find_element(By.CSS_SELECTOR, "input[name='name']").send_keys("12423231242")
 driver.find_element(By.CSS_SELECTOR, "#inlineRadio1").click()
 driver.find_element(By.XPATH, "//input[@type='submit']").click()
 message = driver.find_element(By.CLASS_NAME, "alert-success").text
-
 print(message)
 assert "Success" in message
 
-driver.find_element(By.XPATH,"(//input[@type='text'])[3]").send_keys("222")
+# using index for targeting
+driver.find_element(By.XPATH,"(//input[@type='text'])[3]").send_keys("wfwfax")
 driver.find_element(By.XPATH,"(//input[@type='text'])[3]").clear()
