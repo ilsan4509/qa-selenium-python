ad8ed55559a30c5750e2c9fab93665fd52998c3e
Seonggon <ilsan4509@naver.com>
2025-03-15 15:17:39 +0000

    Add Implicit wait, Explicit wait

--- waiting_explicit.py
+++ waiting_explicit.py
@@ -1,26 +1,28 @@
 import time
-
 from selenium import webdriver
 
 #Chrome driver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
 
 service_obj = Service(r"C:\Users\ilsan\chromedriver-win64\chromedriver.exe")
 driver = webdriver.Chrome(service = service_obj)
 
 # waits in automation testing
-# Implicit Wait
-driver.implicitly_wait(5) # 5 seconds is time out
+# Explicit Wait
+
+# Create WebDriverWait instance
+wait = WebDriverWait(driver, 10)
 
 driver.get("https://rahulshettyacademy.com/seleniumPractise/#/")
 
 driver.find_element(By.CSS_SELECTOR, ".search-keyword").send_keys("ber")
 time.sleep(2) #Waiting for search results
 results = driver.find_elements(By.XPATH, "//div[@class='products']/div")#List[]
-count = len(results)
-# assert count == 3
 
+count = len(results)
 assert count > 0
 for result in results:
     result.find_element(By.XPATH,"div/button").click() #Add all search results to the cart
@@ -28,8 +30,14 @@ for result in results:
 driver.find_element(By.CSS_SELECTOR, "img[alt='Cart']").click()
 driver.find_element(By.XPATH, "//button[text()='PROCEED TO CHECKOUT']").click()
 
+# Wait until .promoCode is loaded
+wait.until(expected_conditions.presence_of_all_elements_located((By.CSS_SELECTOR, ".promoCode")))
+
 driver.find_element(By.CSS_SELECTOR, ".promoCode").send_keys("rahulshettyacademy")
 driver.find_element(By.CSS_SELECTOR, ".promoBtn").click()
+
+# Wait until .promoInfo is loaded
+wait.until(expected_conditions.presence_of_all_elements_located((By.CSS_SELECTOR, ".promoInfo")))
 
 print(driver.find_element(By.CLASS_NAME, "promoInfo").text)
 # input("Press Enter to close the browser...")
