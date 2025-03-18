445fc1614b5cd65e1a951c8fdbf8783218ee55dc
Seonggon <ilsan4509@naver.com>
2025-03-18 23:59:19 +0000

    Add Pytest test execution and grouping with markers

--- python_pytest/test_demo_01.py
+++ python_pytest/test_demo_01.py
@@ -3,10 +3,15 @@
 # Any code should be wrapped in method only
 
 # CMD - pip install pytest, py.test -v -s, py.test test_example.py -v -s
+import pytest
 
-
+@pytest.mark.smoke
 def test_first_program():
     print("Hello")
 
-def test_second_program():
+
+
+# py.test -k creditcard -v -s
+@pytest.mark.xfailv  # when need not report
+def test_second_geet_creditcard():
     print("Meow")
--- python_pytest/test_demo_02.py
+++ python_pytest/test_demo_02.py
@@ -1,13 +1,22 @@
 # Any pytest file should start with test_
 # pytest method names should start with test
 # Any code should be wrapped in method only
+# Method name should have sense
+# -k stands for method names execution, -s logs in out put  -v stands for more info metadata
+# you can run specific file with py.test <filename>
+# you can mark (tag) tests @pytest.mark.smoke and then run with -m
+# you can skip tests with @pytest.mark.skip
 
+import pytest
+
+@pytest.mark.smoke  #py.test -m smoke -v -s
+@pytest.mark.skip # for ignore
 def test_first_program():
-    msg = "Hello"
+    msg = "Hello" #operations
     assert msg == "Hi", "Test failed because strings not match"
 
-
-def test_second_program():
+# py.test -k creditcard -v -s
+def test_second_creditcard():
     a = 4
     b = 6
     assert a+2 == 6, "Addition do not match"
