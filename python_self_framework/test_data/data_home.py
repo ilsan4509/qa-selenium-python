ab9ab8e08e86c5bf22047436522eab05b33a42d5
Seonggon <ilsan4509@naver.com>
2025-03-24 02:54:43 +0000

    Add Excel data integration for Selenium test framework

--- python_self_framework/test_data/data_home.py
+++ python_self_framework/test_data/data_home.py
@@ -1,5 +1,19 @@
-
+import openpyxl
 
 
 class HomeData:
-    test_home_data = [{"name" : "Jayden", "email" : "Seonggon@mail.com", "gender" : "Male"}, {"name" : "Lala", "email" : "Rara@mail.com", "gender" : "Female"}]
+    test_home_data = [{"firstName" : "Jayden", "email" : "Seonggon@mail.com", "gender" : "Male"}, {"fitstName" : "Lala", "email" : "Rara@mail.com", "gender" : "Female"}]
+
+    @staticmethod
+    def get_test_data(test_case_name):
+        book = openpyxl.load_workbook("PythonDemo.xlsx")
+        sheet = book.active
+        Dict = {}
+        for i in range(1,
+                       sheet.max_row + 1):  # to get rows [{"firstName" : "Jayden", "email" : "Seonggon@mail.com", "gender" : "Male"}, {"fitstName" : "Lala", "email" : "Rara@mail.com", "gender" : "Female"}]
+            if sheet.cell(row=i, column=1).value == test_case_name:
+                for j in range(2, sheet.max_column + 1):  # to get columns
+                    # Dict["lastname"]="shetty"
+                    Dict[sheet.cell(row=1, column=j).value] = sheet.cell(row=i, column=j).value
+
+        return [Dict]
--- python_self_framework/tests/test_page_home.py
+++ python_self_framework/tests/test_page_home.py
@@ -10,8 +10,8 @@ class TestHomePage(BaseClass):
     def test_form_submission(self, get_deta):
         log = self.get_logger()
         home_page = HomePage(self.driver)
-        log.info("first name is " + get_deta["name"])
-        home_page.get_name().send_keys(get_deta["name"]) # home_page.get_name().send_keys(get_deta[0])
+        log.info("first name is " + get_deta["firstName"])
+        home_page.get_name().send_keys(get_deta["firstName"]) # home_page.get_name().send_keys(get_deta[0])
         home_page.get_email().send_keys(get_deta["email"]) # home_page.get_email().send_keys(get_deta[1])
         home_page.get_checkbox().click()
         self.select_option_by_text(home_page.get_gender(), get_deta["gender"]) # self.select_option_by_text(home_page.get_gender(), get_deta[2])
@@ -23,6 +23,6 @@ class TestHomePage(BaseClass):
 
         self.driver.refresh()
 
-    @pytest.fixture(params = HomeData.test_home_data)
+    @pytest.fixture(params = HomeData.get_test_data("Testcase2"))
     def get_deta(self, request):
         return request.param
